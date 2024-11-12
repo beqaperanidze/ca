31b5710c6d42b20b896c0860dd9c63b4ce7b88be
beqa peranidze <33373572+beqaperanidze@users.noreply.github.com>
2024-11-12 13:22:08 +0400

    Update Star.java

--- src/main/java/prjnightsky/service/generator/Star.java
+++ src/main/java/prjnightsky/service/generator/Star.java
@@ -1,10 +1,6 @@
 package prjnightsky.service.generator;
 
 
-/**
- * @param rightAscension in hours
- * @param declination    in degrees
- */
 public record Star(String name, double rightAscension, double declination, double magnitude) {
 
 }
